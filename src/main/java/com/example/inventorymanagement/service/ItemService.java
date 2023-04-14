257e3b35f9a66bca73ed4f2b1dd5b34832b42aac
Whcrews1998 <whcrews1998@gmail.com>
2023-04-14 08:21:06 -0400

    Added method to ItemService 'findBySubcategory(Subcategory)' to find all Items that contain that subcategory.

--- src/main/java/com/example/inventorymanagement/service/ItemService.java
+++ src/main/java/com/example/inventorymanagement/service/ItemService.java
@@ -1,8 +1,11 @@
 package com.example.inventorymanagement.service;
 
 import com.example.inventorymanagement.entity.Item;
+import com.example.inventorymanagement.entity.Subcategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemService extends JpaRepository<Item, Integer> {
+import java.util.List;
 
+public interface ItemService extends JpaRepository<Item, Integer> {
+    List<Item> findBySubcategory(Subcategory subcategory);
 }
