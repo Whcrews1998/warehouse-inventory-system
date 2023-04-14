5f6b9c75d5ba1e42a5dc3514866e032ef306d555
Whcrews1998 <whcrews1998@gmail.com>
2023-04-14 09:48:16 -0400

    Added endpoint for listing all subcategories.

--- src/main/java/com/example/inventorymanagement/controller/SubcategoryController.java
+++ src/main/java/com/example/inventorymanagement/controller/SubcategoryController.java
@@ -6,9 +6,12 @@ import com.example.inventorymanagement.service.SubcategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
 
 @Controller
 public class SubcategoryController {
@@ -16,6 +19,12 @@ public class SubcategoryController {
 
     public SubcategoryController(SubcategoryService subcategoryService) {
         this.subcategoryService = subcategoryService;
+    }
+
+    @GetMapping("/subcategories")
+    public ResponseEntity<List<Subcategory>> getAllSubcategories() {
+        List<Subcategory> subcategoryList = subcategoryService.findAll();
+        return new ResponseEntity<>(subcategoryList, HttpStatus.OK);
     }
 
     @PostMapping("/subcategories")
