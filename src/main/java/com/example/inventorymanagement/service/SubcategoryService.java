60fbc351bb68d06075ad219db515e07d5361c88b
Whcrews1998 <whcrews1998@gmail.com>
2023-04-14 10:01:28 -0400

    Added method to Subcategory Service that allows us to find by Category.

--- src/main/java/com/example/inventorymanagement/service/SubcategoryService.java
+++ src/main/java/com/example/inventorymanagement/service/SubcategoryService.java
@@ -1,7 +1,11 @@
 package com.example.inventorymanagement.service;
 
+import com.example.inventorymanagement.entity.Category;
 import com.example.inventorymanagement.entity.Subcategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubcategoryService extends JpaRepository<Subcategory, Integer> {
+    public List<Subcategory> findByCategory(Category category);
 }
