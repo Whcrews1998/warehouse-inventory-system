81839bf1eafb906080a27e6b027a6f6c99425dd5
Whcrews1998 <whcrews1998@gmail.com>
2023-04-25 08:12:08 -0400

    Renamed orderDetails class to orderInfo since our models end with 'Details'

---
+++ src/main/java/com/example/inventorymanagement/entity/OrderInfo.java
@@ -0,0 +1,43 @@
+package com.example.inventorymanagement.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class OrderDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @OneToOne
+    private Item item;
+    private int quantity;
+
+    public OrderDetails() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
