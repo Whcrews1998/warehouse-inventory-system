a832a405bf921d8ec27cd66a71557113c41abc35
Whcrews1998 <whcrews1998@gmail.com>
2023-06-06 07:44:38 -0400

    Created Custom Exception Handler class to handle any new exceptions we create.

---
+++ src/main/java/com/example/inventorymanagement/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.inventorymanagement.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+
+
+@ControllerAdvice
+public class CustomerResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails error = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDate.now());
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ErrorDetails> handleUserNotFound(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails error = new ErrorDetails(ex.getMessage(), request.getDescription(false), LocalDate.now());
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.NOT_FOUND);
+    }
+}
