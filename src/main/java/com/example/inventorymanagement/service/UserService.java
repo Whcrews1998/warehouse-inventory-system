0049556addcf474aee63a9a41a823bc5621319e0
Whcrews1998 <whcrews1998@gmail.com>
2023-06-06 07:58:47 -0400

    Added method to search DB for user by Usernam only. Functiionality used when checking if username already exist when creating new user.

--- src/main/java/com/example/inventorymanagement/service/UserService.java
+++ src/main/java/com/example/inventorymanagement/service/UserService.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserService extends JpaRepository<User, Integer> {
     User findByUsernameAndPassword(String username, String password);
+    User findByUsername(String username);
 }
